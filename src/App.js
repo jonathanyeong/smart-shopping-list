73c2e58643e866cc4cd73bd8c025d904680088af
Jonathan Yeong <jonathanyeong@gmail.com>
2021-04-07 16:14:00 -0700

    Refactor the local storage hook into it's own func

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import getToken from './lib/tokens.js';
+import { useStateWithLocalStorage } from './lib/hooks.js';
 import Nav from './Nav.js';
 import ShoppingList from './ShoppingList.js';
 import ShoppingListForm from './ShoppingListForm.js';
 import './App.css';
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('userToken') || '');
+  const [token, setToken] = useStateWithLocalStorage('userToken');
 
-  useEffect(() => {
-    localStorage.setItem('userToken', token);
-  }, [token]);
+  const generateToken = () => {
+    setToken(getToken());
+  };
 
   return (
     <div className="App">
       <h1>Smart Shopping List</h1>
+
       <Router>
         <Switch>
           <Route path="/list">
@@ -24,6 +32,12 @@ function App() {
             <ShoppingListForm />
           </Route>
         </Switch>
+
+        {token ? (
+          <Redirect to="/list" />
+        ) : (
+          <button onClick={generateToken}>Create New List</button>
+        )}
 
         <Nav />
       </Router>
