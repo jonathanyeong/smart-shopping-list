3520dd21a1e9f36a3c7d91804b5a5aca8811b8bc
Jonathan Yeong <jonathanyeong@gmail.com>
2021-04-06 21:08:26 -0700

    Write to the firestore DB with a form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ShoppingList from './ShoppingList.js';
+import ShoppingListForm from './ShoppingListForm.js';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <h1>Smart Shopping list!</h1>
       <h2>Item list</h2>
       <ShoppingList />
+      <ShoppingListForm />
     </div>
   );
 }
---
+++ src/ShoppingListForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { db } from './lib/firebase.js';
+import './ShoppingListForm.css';
+
+function ShoppingListForm() {
+  const [item, setItem] = useState('');
+
+  const handleSubmit = (event) => {
+    db.collection('shoppingLists').add({
+      items: [item],
+    });
+    event.preventDefault();
+  };
+
+  return (
+    <div class="shopping-list__form-container">
+      <h2>Add Item</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Item To Add:
+          <input
+            type="text"
+            value={item}
+            onChange={(e) => {
+              setItem(e.target.value);
+            }}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default ShoppingListForm;
