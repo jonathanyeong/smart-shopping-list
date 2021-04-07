879b916e96c94dfdb15aa68d0ac01dea12b449b9
Jonathan Yeong <jonathanyeong@gmail.com>
2021-04-07 16:44:14 -0700

    Update shopping list form to include more fields

--- src/ShoppingListForm.js
+++ src/ShoppingListForm.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { db } from './lib/firebase.js';
 import './ShoppingListForm.css';
 
 function ShoppingListForm() {
   const [item, setItem] = useState('');
+  const [buyTime, setBuyTime] = useState(7);
+  const [lastPurchased, setLastPurchased] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem('userToken') || '');
 
   const handleSubmit = (event) => {
-    db.collection('shoppingLists').add({
-      items: [item],
+    db.collection('items').add({
+      name: item,
+      buyTime: buyTime,
+      lastPurchased: lastPurchased,
+      token: token,
     });
     setItem('');
     event.preventDefault();
@@ -18,12 +25,38 @@ function ShoppingListForm() {
       <h2>Add Item</h2>
       <form onSubmit={handleSubmit}>
         <label>
-          Item To Add:
+          Item Name
           <input
             type="text"
             value={item}
             onChange={(e) => {
               setItem(e.target.value);
+            }}
+            required
+          />
+        </label>
+
+        <label>
+          How soon are you likely to buy it again?
+          <select
+            value={buyTime.toString()}
+            onBlur={(e) => {
+              setBuyTime(parseInt(e.target.value));
+            }}
+          >
+            <option value="7">Soon</option>
+            <option value="14">Kind of soon</option>
+            <option value="30">Not Soon</option>
+          </select>
+        </label>
+
+        <label>
+          Last purchased date (Optional)
+          <input
+            type="date"
+            value={lastPurchased}
+            onChange={(e) => {
+              setLastPurchased(e.target.value);
             }}
           />
         </label>
