025d886c48fcdaa90e75c9d6a5708b2d1243971b
Jonathan Yeong <jonathanyeong@gmail.com>
2021-04-06 20:47:14 -0700

    Use react firebase hooks
    And refactor out code into a separate component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { db } from './lib/firebase.js';
+import React from 'react';
+import ShoppingList from './ShoppingList.js';
 import './App.css';
 
 function App() {
-  const [shoppingLists, setShoppingLists] = useState([]);
-
-  useEffect(() => {
-    db.collection('shoppingLists')
-      .get()
-      .then((querySnapshot) => {
-        let lists = [];
-        querySnapshot.forEach((doc) => {
-          lists.push(`${doc.id} => ${doc.data().items}`);
-          console.log(doc.id, ' => ', doc.data());
-        });
-        setShoppingLists(lists);
-      });
-  }, []); // The empty array makes sure that we only call set State once.
-
-  const addShoppingList = () => {
-    db.collection('shoppingLists').add({
-      items: ['beer', 'coffee', 'milk'],
-    });
-  };
-
   return (
     <div className="App">
       <h1>Smart Shopping list!</h1>
       <h2>Item list</h2>
-      <ul>
-        {shoppingLists.map((value, index) => {
-          return <li key={index}>{value}</li>;
-        })}
-      </ul>
-      <button onClick={addShoppingList}>Add a shopping list item!</button>
+      <ShoppingList />
     </div>
   );
 }
